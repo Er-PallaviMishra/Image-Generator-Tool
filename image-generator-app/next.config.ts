4ae209f32806705e6edc3cef5ad7ca8e70920ca3
pallavi mishra <mpallavi210@gmail.com>
2025-08-11 22:27:59 +0530

    metadata added

--- image-generator-app/next.config.ts
+++ image-generator-app/next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false, // true only if you want to deploy despite TS errors
+  },  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "technioz.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "genpic.technioz.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
